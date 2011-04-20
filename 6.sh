b2adc4e19e5302abcbd206c4d2e2ab20140b4044
ivche <iv_cher@e1.ru>
2011-04-20 13:15:34 +0600

    versia 2

--- 6.sh
+++ 6.sh
@@ -4,7 +4,6 @@ base_file=base				# base file name
 test_file=test				# test file name
 cat "./test"|while read tmp	
 do
-	echo -e "new_md5=$new_md5 \ntmp=$tmp "
 	if [ $new_md5 -eq 1 ]
 	then
 		new_md5=0
@@ -12,17 +11,13 @@ do
 	else
 		if [ -n "$tmp" ] 
 		then	
-			echo "$md5_size $tmp"
-			if grep "$md5_size $tmp" $base_file 
+			if ! grep "$md5_size $tmp" $base_file 
 			then
-				echo "exist"  # "$md5_size $tmp"
-			else
-				echo "not exist"
+				echo "$md5_size $tmp"
 			fi
 		else
-			echo "noll"
 			new_md5=1
 		fi
 	echo "	"
-	fi  
+	fi
 done
